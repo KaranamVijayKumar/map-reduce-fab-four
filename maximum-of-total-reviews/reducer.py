e7e5d0f56704f5437eb14cabc73199e28fde76d0
KaranamVijayKumar <karanamvijay960@gmail.com>
2020-02-22 17:01:23 -0600

    for loop for reducer

--- maximum-of-total-reviews/reducer.py
+++ maximum-of-total-reviews/reducer.py
@@ -6,3 +6,19 @@ thisKey = ""
 thisValue = 0.0
 # Skipping the firstline
 next(s)
+# for loop to iterate the file
+for line in s:
+    data = line.strip().split('\t')
+    Hotel_Name, Reviewer_Reviews = data
+    if Hotel_Name != thisKey:
+        if thisKey:
+            # output the last key value pair result
+             r.write(thisKey + '\t' + str(thisValue)+'\n')
+        # start over when changing keys
+        thisKey = Hotel_Name 
+        thisValue = 0.0
+    # apply the aggregation function
+    thisValue += float(Reviewer_Reviews)
+list1 = [thisValue]
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+r.write(str(list1))
