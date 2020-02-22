7f3f00b3ceceb3a58146a68ffc31a2bc98b9eb1e
KaranamVijayKumar <karanamvijay960@gmail.com>
2020-02-22 16:52:17 -0600

    updating mapper code

---
+++ maximum-of-total-reviews/mapper.py
@@ -0,0 +1,6 @@
+# imported csv package to read file
+import csv
+# Reading Hotel_Reviews CSV file into input variable
+input = open("Hotel_Reviews.csv", "r")
+# Writing the mapper output to output1.txt
+output = open("output1.txt", "w")
