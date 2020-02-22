978b5ec13691cee4d9085f0568039402d395c50d
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2020-02-22 14:11:31 -0600

    Created the 01.txt file and output variable to write mapper output in 01.txt

--- count-of-total-negative-reviews/mapper.py
+++ count-of-total-negative-reviews/mapper.py
@@ -1,4 +1,7 @@
 # importing the csv file into mapper
 import csv
-#opening the csv file in read mode and assigning it to variable input
+# opening the csv file in read mode and assigning it to variable input
 input = open("Hotel_Reviews.csv", "r")
+# Created the 01.txt file and output variable to write mapper output in 01.txt
+
+output = open("01.txt", "w")
