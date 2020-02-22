1463b12567dc4911cc2499aa31300ffec7c39949
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2020-02-22 14:31:56 -0600

    Created the 02.txt file and r variable to write reducer output in 02.txt

--- count-of-total-negative-reviews/reducer.py
+++ count-of-total-negative-reviews/reducer.py
@@ -1,2 +1,4 @@
 # opening the 01.txt file in read mode and assigning it to variable s
 s = open("01.txt","r")
+# Created the 02.txt file and r variable to write reducer output in 02.txt
+r = open("02.txt", "w")
