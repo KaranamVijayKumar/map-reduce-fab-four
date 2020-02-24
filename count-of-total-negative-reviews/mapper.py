94030337bd0b105139470a9a58061e2946db054e
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2020-02-24 13:18:31 -0600

    updated the path of csv file in mapper.py.

--- count-of-total-negative-reviews/mapper.py
+++ count-of-total-negative-reviews/mapper.py
@@ -1,7 +1,7 @@
 # importing the csv file into mapper
 import csv
 # opening the csv file in read mode and assigning it to variable input
-input = open("Hotel_Reviews.csv", "r")
+input = open("..\\data\\Hotel_Reviews.csv", "r")
 # Created the 01.txt file and output variable to write mapper output in 01.txt
 output = open("01.txt", "w")
 # Created count variable to count the number of lines in 01.txt
